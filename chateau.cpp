2f139460e3fd3ddf9844c09ed1af59efcce1a900
Aquassaut <jeremy.autran@gmail.com>
2012-11-19 22:42:06 +0100

    Commit initial

---
+++ chateau.cpp
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+}
+
+// On a deux chateaux, un en (-p, 0), un en (p, 0)
+// On a une colline au milieu, sa courbe vaut : y = randHauteur*(1-On a du vent pow((2x/randLargeur, 2)))
+//on a un vent de valeur random entre -ventMax et ventMax
+//Ca devrait être suivante pour savoir quelles variables locales on va devoir créer :
+
+//initent)
+//
+
